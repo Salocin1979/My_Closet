d282379f9ececa31e9258c02e3e3e3a901d44483
treelochna sobrun <tsobrun@gmail.com>
2021-10-13 19:49:59 +0400

    method to destroy

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,7 +4,7 @@ class BookingsController < ApplicationController
     def destroy
         @booking = Booking.find(params[:id])
         @booking.destroy
-        #redirect_to outfits_path(@review.list)
+        #redirect_to outfits_path(@booking.list)
       end
 
    end
