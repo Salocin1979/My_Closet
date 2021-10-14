1c4b65bdea786594dfdf6c69f818939fbb10ca99
fredericfdupuche <dupuchefrederic@gmail.com>
2021-10-14 19:05:16 +0400

    latest

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+
+def show
+    @booking = Booking.find(params[:id])
+
+  end
+
+def create
+    @booking = Booking.new(Booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+def booking_params
+    params.require(:booking).permit(:user, :booking_date, :outfit)
+  end
 end
