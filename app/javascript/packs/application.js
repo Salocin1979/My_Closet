fbaa51614d4258471ae039722b69fb7063848c76
frederic dupuche <dupuchefrederic@gmail.com>
2021-10-23 14:52:07 +0400

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { initFlatPickr } from '../plugins/flatpickr';
+import { initDate } from '../plugins/flatpickr';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,5 +32,5 @@ import { initFlatPickr } from '../plugins/flatpickr';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initFlatPickr();
+  initDate();
 });
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,19 +2,19 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-const initFlatpickr = () => {
+const initDate = () => {
   const bookingForm = document.getElementById('new_booking');
   console.log(bookingForm)
   if (bookingForm) {
-    const bookings = JSON.parse(bookingForm.dataset.bookings);
+    // const bookings = JSON.parse(bookingForm.dataset.bookings);
     flatpickr("#range_start", {
       plugins: [new rangePlugin({ input: "#range_end"})],
       minDate: "today",
       inline: true,
       dateFormat: "Y-m-d",
-      "disable": bookings,
+      // "disable": bookings,
     })
   }
 };
 
-export { initFlatpickr };
+export { initDate };
